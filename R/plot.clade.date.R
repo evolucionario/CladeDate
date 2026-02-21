1005ee0df93932abbd304249d109039f19534bd6
Santiago Claramunt <9385439+evolucionario@users.noreply.github.com>
2026-02-20 21:08:27 -0600

    Update plot.clade.date.R

--- R/plot.clade.date.R
+++ R/plot.clade.date.R
@@ -27,7 +27,7 @@
 #' ## Plot
 #' plot(X)
 #'
-#' @author Santiago Claramunt, \email{s.claramunt@@utoronto.ca}
+#' @author Santiago Claramunt, \email{sclaramu@@uno.edu}
 
 #' @references
 
@@ -49,8 +49,6 @@ plot.clade.date <- function(object, breaks="FD", ...) {
 		# Set x limits
 		XX <- c(max(0, min(ages)-min(ages)/8), quantile(rA, prob=0.99))
 		
-		par(lend=1, las=1, mar=c(5,5,1,1), ...)
-
 		HIST <- hist(rA, breaks=breaks, freq=FALSE, xlim=XX, col=rgb(.25,.7,.9,0.5), border="#4DCCFF", xlab="Time", main="", yaxs="i")
 
 
@@ -64,26 +62,26 @@ plot.clade.date <- function(object, breaks="FD", ...) {
 						
 			if(object$PDFfit.model=="lognormal") {
 				curve(dlnorm(x-max(ages[,1]), meanlog=object$PDFfit$estimate[1], sdlog=object$PDFfit$estimate[2]), n=301, from=max(ages[,1]), to=XX[2], col="#CC6600", lwd=3, add=TRUE, xpd=TRUE)
-				legend("right", legend="log-normal density", text.col="#CC6600", bty="n")
+				mtext("Log-normal density", col="#CC6600", line=1)
 			}
 
 			if(object$PDFfit.model=="gamma") {
 				curve(dgamma(x-max(ages[,1]), shape=object$PDFfit$estimate[1], rate=object$PDFfit$estimate[2]), n=301, from=max(ages[,1]), to=XX[2], col="#CC6600", lwd=3, add=TRUE, xpd=TRUE)
-				legend("right", legend="gamma density", text.col="#CC6600", bty="n")
+				mtext("Gamma density", col="#CC6600", line=1)
 			}
 
 			if(object$PDFfit.model=="exponential") {
 				curve(dexp(x-max(ages[,1]), rate=object$PDFfit$estimate[1]), n=301, from=max(ages[,1]), to=XX[2], col="#CC6600", lwd=3, add=TRUE, xpd=TRUE)
-				legend("right", legend="exponential density", text.col="#CC6600", bty="n")
+				mtext("Exponential density", col="#CC6600", line=1)
 			}
 			if(object$PDFfit.model=="skewnormal") {
 				curve(sn::dsn(x, xi=object$PDFfit.param["xi"], omega=object$PDFfit.param["omega"], alpha=object$PDFfit.param["alpha"]), n=301, from=XX[1], to=XX[2], col="#CC6600", lwd=3, add=TRUE, xpd=TRUE)
-				legend("right", legend="skew-normal density", text.col="#CC6600", bty="n")
+				mtext("Skew-normal density", col="#CC6600", line=1)
 			}
 		
 			if(object$PDFfit.model=="skewstudent") {
 			curve(sn::dst(x, xi=object$PDFfit.param["xi"], omega=object$PDFfit.param["omega"], alpha=object$PDFfit.param["alpha"], nu=object$PDFfit.param["nu"]), n=301, from=XX[1], to=XX[2], col="#CC6600", lwd=3, add=TRUE, xpd=TRUE)
-			legend("right", legend="skew-Student density", text.col="#CC6600", bty="n")
+			mtext("Skew-Student density", col="#CC6600", line=1)
 			}
 		}
 		
@@ -94,6 +92,6 @@ plot.clade.date <- function(object, breaks="FD", ...) {
 
 	points(quantile(rA, prob=c(0.5,0.95)), c(0,0), pch=c(17,18), col="#0078B3", xpd=TRUE, cex=1.2)
 		
-	legend("topleft", legend=c("fossil ages","MC distribution", "median","95% quantile"), pch=c(17,22, 17,18), col=c("black","#4DCCFF","#0078B3","#0078B3"), pt.bg=c("black",rgb(.25,.7,.9,0.5)), bty="n") # inset=c(0, -0.1), xpd=TRUE
+	legend("topright", legend=c("fossil ages","MC distribution", "median","95% quantile"), pch=c(17,22, 17,18), col=c("black","#4DCCFF","#0078B3","#0078B3"), pt.bg=c("black",rgb(.25,.7,.9,0.5)), bty="n") # inset=c(0, -0.1), xpd=TRUE
 	
 }	
